10de03e8628444e4a835f8a8299ba02980b94d3f
Dariusz Okonski <darek200180@gmail.com>
2020-05-27 13:02:27 +0100

    basic setup

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello wold');
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running at ${port}...`);
+})

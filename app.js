21a16d4857dbbb82736aec7af0702bc0631c8a7f
Dariusz Okonski <darek200180@gmail.com>
2020-05-27 15:51:24 +0100

    build read data from mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const Actor = require('./models/actor');
 
 const indexRouter = require('./routes/actors/index');
 const mongoConnection = require('./models/connection');
@@ -11,8 +12,41 @@ app.set('view engine', 'ejs');
 
 app.use(indexRouter);
 
-const port = 3000;
 
+// seed();
+// listener ===================================
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running at ${port}...`);
 })
+
+//seed ================================================
+function seed() {
+    const actors = [
+        {
+            name: 'Arnold', 
+            surname: 'Schwarzenegger', 
+            country: 'Austria', 
+            picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Arnold_Schwarzenegger_edit%28ws%29.jpg/528px-Arnold_Schwarzenegger_edit%28ws%29.jpg'
+        },
+        {
+            name: 'Sylwester', 
+            surname: 'Stallone', 
+            country: 'USA', 
+            picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Sylvester_Stallone_%287588423560%29.jpg/800px-Sylvester_Stallone_%287588423560%29.jpg'
+        },
+        {
+            name: 'Dolph', 
+            surname: 'Lundgreen', 
+            country: 'Sweden', 
+            picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Dolph_Lundgren_%2815186029635%29.jpg/428px-Dolph_Lundgren_%2815186029635%29.jpg'
+        }
+    ]
+
+    actors.forEach(actor => {
+        Actor.create(actor);
+    })
+
+
+}
+    
--- routes/actors/index.js
+++ routes/actors/index.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-
-const actors = [
-    {
-        _id: 1, 
-        name: 'Arnold', 
-        surname: 'Schwarzenegger', 
-        country: 'Austria', 
-        picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Arnold_Schwarzenegger_edit%28ws%29.jpg/528px-Arnold_Schwarzenegger_edit%28ws%29.jpg'},
-    {_id: 2, name: 'Sylwester', surname: 'Stallone', country: 'USA', picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Sylvester_Stallone_%287588423560%29.jpg/800px-Sylvester_Stallone_%287588423560%29.jpg'},
-    {_id: 3, name: 'Dolph', surname: 'Lundgreen', country: 'Sweden', picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Dolph_Lundgren_%2815186029635%29.jpg/428px-Dolph_Lundgren_%2815186029635%29.jpg'}
-]
+const Actor = require('../../models/actor');
 
 router.get('/', (req, res) => {
     res.render('landing');
 });
 
 router.get('/actors', (req, res) => {
-    res.render('actors/index', {
-        actors
-    });
+    Actor.find({}, (err, foundActors) => {
+        if(err) {
+            console.log('ERROR')
+        } else {
+            res.render('actors/index', {
+                actors: foundActors
+            });
+        }
+    })
+
+
+
 });
 
 router.get('/actors/:id', (req, res) => {

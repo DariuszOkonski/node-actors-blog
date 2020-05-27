388794b91af35103e547b530c849fa2b2826153a
Dariusz Okonski <darek200180@gmail.com>
2020-05-27 17:00:52 +0100

    add model comment

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    author: String,
+    content: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports =  mongoose.model('comment', commentSchema);
